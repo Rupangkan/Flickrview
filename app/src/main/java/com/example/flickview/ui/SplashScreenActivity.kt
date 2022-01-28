818d895bd11b18afab70794581bef00ef19620c9
Rupangkan <every1isnotrupangkan@gmail.com>
2022-01-28 15:11:25 +0530

    Added splash screen

---
+++ app/src/main/java/com/example/flickview/ui/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.example.flickview.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.flickview.MainActivity
+import com.example.flickview.R
+
+class SplashScreenActivity: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val imageView: ImageView = findViewById(R.id.imageViewNoted)
+        val text: TextView = findViewById(R.id.text)
+        text.alpha = 0f
+        imageView.alpha = 0f
+        text.animate().setDuration(800).alpha(1f).withEndAction{
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+        imageView.animate().setDuration(800).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+
+        }
+
+    }
+}
